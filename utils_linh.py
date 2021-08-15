4c3c776edcef20fbe28e7d369775e0ac9055aff9
PhamLinh97 <86882233+PhamLinh97@users.noreply.github.com>
2021-08-15 17:44:17 +0700

    Update utils_linh.py

--- utils_linh.py
+++ utils_linh.py
@@ -1,1 +1,30 @@
+import requests
+from peewee import *
+from utils_db_linh import *
+from requests.models import codes
 
+def get_score(sbd):
+	# sbd = '02000001'
+	url = f'https://diemthi.vnanet.vn/Home/SearchBySobaodanh?code={sbd}&nam=2021'
+
+	headers = {
+		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/96.0.230 Chrome/90.0.4430.230 Safari/537.36',
+		'X-Requested-With' : 'XMLHttpRequest'
+	}
+
+	res = requests.get(url, headers = headers).json()
+	try:
+		toan = res['result'][0]['Toan']
+		van = res['result'][0]['NguVan']
+		anh = res['result'][0]['NgoaiNgu']
+	except:
+		toan = -1
+		van = -1
+		anh = 1
+	row = btvn_linh(sbd = sbd, toan = toan, van = van, anh = anh, is_run = 1)
+	row.save()
+	return toan,van,anh
+
+if __name__=="__main__":
+	sbd = '02010000'
+	toan,van,anh = get_score(sbd)
